26ad6afb0872a402404927b085e918b91cb191f8
Lukas Corbitt <movalley5@gmail.com>
2019-12-27 01:06:20 -0700

    Added styling

--- app.js
+++ app.js
@@ -3,7 +3,36 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var campgrounds = [ { name: 'Salmon Creek' }, { name: 'Granite Hill' }, { name: 'Mountain Goat Springs' } ];
+var campgrounds = [
+	{
+		name: 'Salmon Creek',
+		image: 'https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722878d09f48c65d_340.jpg'
+	},
+	{
+		name: 'Granite Hill',
+		image:
+			'https://www.photosforclass.com/download/pixabay-1189929?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c722878d09f48c65d_960.jpg&user=Noel_Bauza'
+	},
+	{
+		name: 'Mountain Goat Springs',
+		image:
+			'https://www.photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c722878d09f48c65d_960.jpg&user=Pexels'
+	},
+	{
+		name: 'Salmon Creek',
+		image: 'https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722878d09f48c65d_340.jpg'
+	},
+	{
+		name: 'Granite Hill',
+		image:
+			'https://www.photosforclass.com/download/pixabay-1189929?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c722878d09f48c65d_960.jpg&user=Noel_Bauza'
+	},
+	{
+		name: 'Mountain Goat Springs',
+		image:
+			'https://www.photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c722878d09f48c65d_960.jpg&user=Pexels'
+	}
+];
 
 app.set('view engine', 'ejs');
 
@@ -21,8 +50,9 @@ app.get('/campgrounds/new', function(req, res) {
 
 app.post('/campgrounds', function(req, res) {
 	var name = req.body.name;
-	var newCampground = { name: name };
-	campgrounds.push(newCampground);
+	var image = req.body.image;
+	var newCampground = { name: name, image: image };
+	campgrounds.unshift(newCampground);
 	res.redirect('campgrounds');
 });
 
