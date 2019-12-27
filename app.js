199a21a7aa74739e6d048db40e6029372f8fc3e2
Lukas Corbitt <movalley5@gmail.com>
2019-12-26 21:08:45 -0700

    Added initial routes

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var campgrounds = [ { name: 'Salmon Creek' }, { name: 'Granite Hill' }, { name: 'Mountain Goat Springs' } ];
+
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-	res.send('Working');
+	res.render('landing');
+});
+
+app.get('/campgrounds', function(req, res) {
+	res.render('campgrounds', { campgrounds: campgrounds });
+});
+
+app.get('/campgrounds/new', function(req, res) {
+	res.render('new.ejs');
+});
+
+app.post('/campgrounds', function(req, res) {
+	var name = req.body.name;
+	var newCampground = { name: name };
+	campgrounds.push(newCampground);
+	res.redirect('campgrounds');
 });
 
 app.listen(3000, function() {
