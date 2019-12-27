522aac0ffed9629a74b3101c7162e80bec459cde
Lukas Corbitt <movalley5@gmail.com>
2019-12-27 16:23:05 -0700

    Added database and removed hardcoded data

--- app.js
+++ app.js
@@ -1,49 +1,55 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
-
-var campgrounds = [
-	{
-		name: 'Salmon Creek',
-		image: 'https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722878d09f48c65d_340.jpg'
-	},
-	{
-		name: 'Granite Hill',
-		image:
-			'https://www.photosforclass.com/download/pixabay-1189929?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c722878d09f48c65d_960.jpg&user=Noel_Bauza'
-	},
-	{
-		name: 'Mountain Goat Springs',
-		image:
-			'https://www.photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c722878d09f48c65d_960.jpg&user=Pexels'
-	},
-	{
-		name: 'Salmon Creek',
-		image: 'https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722878d09f48c65d_340.jpg'
-	},
-	{
-		name: 'Granite Hill',
-		image:
-			'https://www.photosforclass.com/download/pixabay-1189929?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c722878d09f48c65d_960.jpg&user=Noel_Bauza'
-	},
-	{
-		name: 'Mountain Goat Springs',
-		image:
-			'https://www.photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c722878d09f48c65d_960.jpg&user=Pexels'
-	}
-];
-
 app.set('view engine', 'ejs');
 
+// DB SETUP
+mongoose
+	.connect('mongodb://localhost/yelp_camp', {
+		useUnifiedTopology: true,
+		useNewUrlParser: true
+	})
+	.then(() => console.log('DB Connected!'))
+	.catch((err) => {
+		console.log('DB Connection Error: ${err.message}');
+	});
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create({
+// 	name: 'Salmon Creek',
+// 	image:
+// 		'https://www.photosforclass.com/download/pixabay-1846142?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c72287fd79245c159_960.jpg&user=Pexels'
+// });
+
+// Landing Page
 app.get('/', function(req, res) {
 	res.render('landing');
 });
 
+// Get all campgrounds from db
 app.get('/campgrounds', function(req, res) {
-	res.render('campgrounds', { campgrounds: campgrounds });
+	Campground.find({}, (err, allCampgrounds) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('campgrounds', { campgrounds: allCampgrounds });
+		}
+	});
 });
 
+// Create new campground
 app.get('/campgrounds/new', function(req, res) {
 	res.render('new.ejs');
 });
@@ -52,10 +58,17 @@ app.post('/campgrounds', function(req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCampground = { name: name, image: image };
-	campgrounds.unshift(newCampground);
-	res.redirect('campgrounds');
+	// Create a new campground and save to db
+	Campground.create(newCampground, (err, newlyCreated) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect('/campgrounds');
+		}
+	});
 });
 
+// Server listener
 app.listen(3000, function() {
 	console.log('YelpCamp server has started!');
 });
